86b16875330f054e0e026413d4731130fe32f2cf
Ben Brown <bdbrown4@eagles.usi.edu>
2020-03-29 10:05:55 -0400

    checking in web layout

---
+++ src/modules/item/item.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ItemRoutingModule } from './item-routing.module';
+import { ItemComponent } from './item.component';
+
+
+@NgModule({
+  declarations: [ItemComponent],
+  imports: [
+    CommonModule,
+    ItemRoutingModule
+  ]
+})
+export class ItemModule { }
